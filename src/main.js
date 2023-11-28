fa441823513ada50c7185dc4b05004fd3259f8b2
André Torquato <ats.torquato1@gmail.com>
2023-11-27 22:44:04 -0300

    migrate project to vite

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./routes";
-// import { VueFire } from "vuefire";
+import { VueFire, VueFireAuth } from "vuefire";
 
-// import { firebaseApp } from "./firebase";
+import { firebaseApp } from "./firebase";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "./styles.css";
 
 const app = createApp(App);
 
 app.use(router);
-// app.use(VueFire, {
-//   firebaseApp,
-//   modules: [],
-// })
+app.use(VueFire, {
+  firebaseApp,
+  modules: [
+    VueFireAuth(),
+  ],
+})
 
 
 app.mount("#app");
